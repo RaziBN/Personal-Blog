d3b9a4a877f4e2576dedb314d396b5dc3e4c1b48
Razi Benvidi <raziyehes859@gmail.com>
2024-07-29 20:25:45 -0400

    Error fixed

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,38 +2,41 @@ import { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import NextAuth from "next-auth/next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const authOptions: AuthOptions = {
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID ?? "",
-      clientSecret: process.env.GITHUB_SECRET ?? "",
+      clientId: isProduction
+        ? process.env.GITHUB_ID_PROD ?? ""
+        : process.env.GITHUB_ID ?? "",
+      clientSecret: isProduction
+        ? process.env.GITHUB_SECRET_PROD ?? ""
+        : process.env.GITHUB_SECRET ?? "",
     }),
   ],
   callbacks: {
     async signIn({ user }) {
-      console.log("SignIn callback triggered");
-      if (user) {
-        console.log("User:", user);
-      } else {
-        console.log("User not found");
-      }
-
       if (user.name === "Razi Benvidi") {
-        console.log("User allowed:", user);
         return true;
       } else {
-        console.log("User not allowed:", user);
         return false;
       }
     },
     async session({ session, token }: any) {
-      console.log("Session callback triggered");
       session.user.name = `${session?.user?.name}_${token?.sub}`;
       return session;
     },
   },
-
   secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/auth/signin",
+    signOut: "/auth/signout",
+    error: "/auth/error",
+    verifyRequest: "/auth/verify-request",
+    newUser: undefined,
+  },
+  debug: true,
 };
 
 const nextAuth = NextAuth(authOptions);
